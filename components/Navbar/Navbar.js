9abb3eb6ca5bca2530a323842e3bc3d2ecf9037b
Rahul T <rahulmanojcoder@gmail.com>
2022-02-16 23:30:32 +0530

    initial setup

---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import styles from './Navbar.module.scss'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+      <nav className={styles.navbar}>
+      </nav>
+    )
+}
+
+export default Navbar
