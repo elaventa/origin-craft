a91cf3bf6a5f20927081a4d3657e9c0f4c6f2167
SuryaRamanath <vrsurya80@gmail.com>
2022-07-09 16:38:02 +0530

    footer structure

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,8 +1,48 @@
 import React from 'react'
+import styles from './Footer.module.scss'
+import logo from "../../assets/logo.png"
+import Image from 'next/image'
+
+
 
 const Footer = () => {
   return (
-    <div>This is footer</div>
+    <div className={styles.footer}>
+      <div className={styles.col1}>
+        <li>
+        <Image className={styles.logo} src={logo} alt="logo"/>
+
+        </li>
+        <li>
+          At Origin Craft, we provide creative and professionally made customized Leather & Paper- Boxes & Holders for any product.
+        </li>
+      </div>
+      <div className={styles.col2}>
+        <li>
+          Home
+        </li>
+        <li>
+          Products
+        </li>
+        <li>
+          About
+        </li>
+        <li>
+          Contact Us
+        </li>
+      </div>
+      <div className={styles.col3}>
+        <li>        Contact: +971 557206035, +971 528779096
+</li>
+<li>
+  Email: origincraft.rak@gmail.com
+</li>
+<li>
+  Address :PO Box 6315
+  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+</li>
+      </div>
+    </div>
   )
 }
 
