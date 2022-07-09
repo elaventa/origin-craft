0df91d1271209f547cc23b3606d12fa68c543767
SuryaRamanath <vrsurya80@gmail.com>
2022-07-09 22:42:28 +0530

    about and contact structure

---
+++ components/AboutUs/AboutUs.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./AboutUs.module.scss";
+
+const AboutUs = () => {
+  return (
+    <div className={styles.aboutus}>
+      <div>
+        <p className={styles.about}>About us</p>
+
+        <p className={styles.descr}>
+          Beautiful & Creative Box is an exceptional way to boost your product
+          packaging to a whole new level. Leather is a material that is commonly
+          associated with luxury, which helps you create the perceived quality
+          you desire. With Origin Craft, we can provide you professionally made
+          leather & Paper Boxes for any product.
+        </p>
+      </div>
+      <div className={styles.blank}></div>
+    </div>
+  );
+};
+
+export default AboutUs;
--- components/ReachOut/ReachOut.js
+++ components/ReachOut/ReachOut.js
@@ -1,12 +1,45 @@
-import React from 'react'
-import styles from './ReachOut.module.scss'
+import React from "react";
+import styles from "./ReachOut.module.scss";
+import { FaWhatsapp, FaInstagram } from "react-icons/fa";
+import Map from "../Map/Map";
 
-const ReachOut = () => {
-
-
-    return(
-        <div></div>
-    );
+const AboutUs = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>
+        <p className={styles.reach}>Reach Out Us</p>
+        <p className={styles.reach2}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium,
+          ex!
+        </p>
+      </div>
+      <div className={styles.contact}>
+        <div className={styles.details}>
+          <p>
+            {" "}
+            <span>Contact: </span> +971 557206035, +971 528779096
+          </p>
+          <p>
+            {" "}
+            <span>Email: </span> origincraft.rak@gmail.com
+          </p>
+          <p>
+            {" "}
+            <span>Adress: </span> P.O Box 6315 <br /> Al Qusaidat, Near Lamp
+            Round About, Ras Al Khaimah,
+            <br /> United Arab Emirates{" "}
+          </p>
+          <div>
+            <FaWhatsapp />
+            <FaInstagram />
+          </div>
+        </div>
+        <div className={styles.map}>
+          <Map />
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default ReachOut;
+export default AboutUs;
