4dfabd097e65ad4ff3994bad95b7f311b38b3c11
Rahul T <rahulmanojcoder@gmail.com>
2022-07-09 22:57:41 +0530

    image change

--- data/products.js
+++ data/products.js
@@ -1,49 +1,49 @@
 const products = [
     {   
         id: 1,
-        url: "https://rahult.in/static/859034dbb7d9a358a0c3b21aedde4705/ee604/dp.png",
+        url: "https://res.cloudinary.com/dpurlepvo/image/upload/v1657268702/cld-sample-5.jpg",
         name: "a",
         category: "box"
     },
     {
         id: 2,
-        url: "https://rahult.in/static/859034dbb7d9a358a0c3b21aedde4705/ee604/dp.png",
+        url: "https://res.cloudinary.com/dpurlepvo/image/upload/v1657268702/cld-sample-5.jpg",
         name: "a",
         category: "box"
     },
     {
         id: 3,
-        url: "https://rahult.in/static/859034dbb7d9a358a0c3b21aedde4705/ee604/dp.png",
+        url: "https://res.cloudinary.com/dpurlepvo/image/upload/v1657268702/cld-sample-5.jpg",
         name: "a",
         category: "purse"
     },
     {
         id: 4,
-        url: "https://rahult.in/static/859034dbb7d9a358a0c3b21aedde4705/ee604/dp.png",
+        url: "https://res.cloudinary.com/dpurlepvo/image/upload/v1657268702/cld-sample-5.jpg",
         name: "a",
         category: "purse"
     },
     {   
         id: 5,
-        url: "https://rahult.in/static/859034dbb7d9a358a0c3b21aedde4705/ee604/dp.png",
+        url: "https://res.cloudinary.com/dpurlepvo/image/upload/v1657268702/cld-sample-5.jpg",
         name: "a",
         category: "box"
     },
     {
         id: 6,
-        url: "https://rahult.in/static/859034dbb7d9a358a0c3b21aedde4705/ee604/dp.png",
+        url: "https://res.cloudinary.com/dpurlepvo/image/upload/v1657268702/cld-sample-5.jpg",
         name: "a",
         category: "box"
     },
     {
         id: 7,
-        url: "https://rahult.in/static/859034dbb7d9a358a0c3b21aedde4705/ee604/dp.png",
+        url: "https://res.cloudinary.com/dpurlepvo/image/upload/v1657268702/cld-sample-5.jpg",
         name: "a",
         category: "purse"
     },
     {
         id: 8,
-        url: "https://rahult.in/static/859034dbb7d9a358a0c3b21aedde4705/ee604/dp.png",
+        url: "https://res.cloudinary.com/dpurlepvo/image/upload/v1657268702/cld-sample-5.jpg",
         name: "a",
         category: "purse"
     },
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['rahult.in'],
+    domains: ['res.cloudinary.com'],
   },
 }
