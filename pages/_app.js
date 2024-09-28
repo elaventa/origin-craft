04ad1ab24dccfdacf6a1c2ccdc5f6f5bb419de45
Rahul T <rahulmanojcoder@gmail.com>
2024-09-28 19:21:05 +0530

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,24 @@
 import "../styles/globals.scss";
+import Script from "next/script";
 
 import { DefaultSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }) {
+
     return (
         <>
+            <Script id="next"
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=G-4CGFM9BWS4`}>
+            </Script>
+            <Script id="next">
+                {
+                    `window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-4CGFM9BWS4');console.log('hi');`
+                }
+            </Script>
             <DefaultSeo
                 title="Origin Craft UAE"
                 description="At Origin Craft, we provide creative and professionally made customized Leather & Paper- Boxes & Holders for any product."
